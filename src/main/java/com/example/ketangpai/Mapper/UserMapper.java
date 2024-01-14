884d3cb5a09e8449b6f57c24901c3245f5d3ba43
cxy <chenxinyi@chenxinyideMBP.lan>
2024-01-14 14:49:30 +0800

    Changes

--- src/main/java/com/example/ketangpai/Mapper/UserMapper.java
+++ src/main/java/com/example/ketangpai/Mapper/UserMapper.java
@@ -4,9 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-    public User query(User user);
 
-    public int add(User user);
 }
 
 
--- src/main/java/com/example/ketangpai/service/Impl/UserOperateImpl.java
+++ src/main/java/com/example/ketangpai/service/Impl/UserOperateImpl.java
@@ -8,10 +8,9 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
-import static com.example.ketangpai.Util.JwtUtil.createJWt;
 
 @Component
-public class UserOperateImpl implements UserOperate {
+public class UserOperateImpl {
 
     @Autowired
     public UserMapper userMapper;
@@ -22,42 +21,7 @@ public class UserOperateImpl implements UserOperate {
         userOperate = this;
         userOperate.userMapper = this.userMapper;
     }
-    public  String addUser(User user){
-     User user1 = userOperate.userMapper.query(user);
-     if (user1 != null){
-         return "false";
-     }else {
-          Integer i = userOperate.userMapper.add(user);
-          return "true";
-     }
-    }
-    public String login(User user){
-        System.out.println(user);
-        User user1 = userOperate.userMapper.query(user);
-        if (user1 == null){
-            return JSON.toJSONString("null");
-        }else if(user.password.equals(user1.password)){
-            user1.token = createJWt(user);
-            System.out.println(user1);
-            return JSON.toJSONString(user1);
-        }else {
-            return JSON.toJSONString("false");
-        }
-    }
-    public String getData(User user){
-//        if (user.token == null || user.token == ""){
-//            return "false";
-//        }
-//        try {
-//            Jws<Claims> claimsJws = Jwts.parser().setSigningKey("admin").parseClaimsJws(user.token);
-            User user1 = userOperate.userMapper.query(user);
-            user1.password = "";
-//            user1.token = createJWt(user);
-            return JSON.toJSONString(user1);
-//        }catch (ExpiredJwtException e){
-//            return "false";
-//        }
-    }
+
 }
 
 
